36b6ac62f67a74a9e5fcc36087e87a3d396bbc8e
Zachary Cutler <zachlcutler@gmail.com>
2016-04-20 16:39:16 -0400

    modified spec helper to get rid of deprecation warnings

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,3 +1,4 @@
 class TodoList < ActiveRecord::Base
   validates :title, presence: true, length: {minimum: 3}
+  validates :description, presence: true, length: {minimum: 3}
 end
--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -59,4 +59,22 @@ describe "Creating todo lists" do
     visit "/todo_lists"
     expect(page).to_not have_content("This is what I'm doing today")
   end
+
+  it "displays error when todo list has no description" do
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: "Hi"
+    fill_in "Description", with: ""
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("This is what I'm doing today")
+  end
 end
